63a132f219d57f11021c62af68215dc618109f25
HP <HP@HP-PC>
2023-06-26 10:55:28 +0530

    new code

--- For java/src/test/Loop.java
+++ For java/src/test/Loop.java
@@ -1,5 +1,19 @@
 package test;
 
 public class Loop {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int b= 6 ; b >=1 ; b--)  //line
+        {
+        	 for(int a = 1; a <= b ; a++)  // star
+        	 {
+        		 System.out.print("*");
+        	 }
+        	 System.out.println();
+        	 
+        	 }
+        }
 
 }
